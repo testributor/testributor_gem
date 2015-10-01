f6dff2afcbcc506f97f0513b5c5e9770f0f19a7a
Dimitris Karakasilis <jimmykarily@gmail.com>
2015-10-01 16:45:27 +0300

    Refactor Testributor to module, inject our reporter in tests

--- lib/testributor/test_job_file.rb
+++ lib/testributor/test_job_file.rb
@@ -1,69 +1,79 @@
 # This is a wrapper class around the test_job_file response from testributor
-class TestJobFile
-  attr_reader :repo, :commit_sha, :file_name, :build_commands
+module Testributor
+  class TestJobFile
+    attr_reader :repo, :commit_sha, :file_name, :build_commands
 
-  def initialize(file_response, repo, build_commands='')
-    @commit_sha = file_response["test_job"]["commit_sha"]
-    @file_name = file_response["file_name"]
-    @repo = repo
-    @build_commands = build_commands
-  end
-
-  def run
-    if commit_changed?
-      checkout_to_job_commit
-      setup_test_environment
+    def initialize(file_response, repo, build_commands='')
+      @commit_sha = file_response["test_job"]["commit_sha"]
+      @file_name = file_response["file_name"]
+      @repo = repo
+      @build_commands = build_commands
     end
 
-    # TODO: Change the reporter or do something so we can get the output
-    # structured
-    #`bin/rake test #{file_name}`
-    log "Running test file #{file_name}"
-    log `bin/rake test #{file_name}`
+    def run
+      if commit_changed?
+        checkout_to_job_commit
+        setup_test_environment
+      end
 
-    report_results
-  end
+      # Unless already required the reporter on this file
+      # inject our own reporter by requiring on the top of the file to run
+      unless File.open(file_name, &:gets) =~ /testributor\/reporter/
+        log "Injecting our reporter"
+        File.write(
+          file_name, "require 'testributor/reporter'\n" + File.read(file_name))
+      end
+      log "Running test file #{file_name}"
+      log `bin/rake test #{file_name}`
 
-  # Yields the given block is test_job commit is different from the current one
-  def commit_changed?
+      report_results
+    end
+
     # Use only the first 6 characters from each SHA1 to compare
-    current_commit_sha[0..5] != commit_sha[0..5]
-  end
-
-  private
-
-  def current_commit_sha
-    Dir.chdir(Testributor::PROJECT_DIR) do
-      `git rev-parse HEAD`.strip
+    def commit_changed?
+      current_commit_sha[0..5] != commit_sha[0..5]
     end
-  end
 
-  # TODO: Handle the following case gracefully:
-  # Testributor#run has already fetched the repo if the commit is not known.
-  # Still, there might be a case that the commit cannot be found even after
-  # pulling the repo. E.g. The history was rewritten somehow (do the commits
-  # get lost then?) or the repo has been reset (deleted and repushed). This is
-  # an edge case but our worker should probably inform katana about this (so
-  # katana can notify the users).
-  def checkout_to_job_commit
-    log "Checking out #{commit_sha}"
-    repo.checkout(commit_sha)
-  end
+    private
 
-  # Create test database, install needed gems and run any custom build scripts
-  def setup_test_environment
-    log "Setting up environment"
-    Dir.chdir(Testributor::PROJECT_DIR) do
-      log `#{build_commands}` if build_commands && build_commands != ''
+    def current_commit_sha
+      Dir.chdir(Testributor::Worker::PROJECT_DIR) do
+        `git rev-parse HEAD`.strip
+      end
     end
-  end
 
-  def report_results
-    log "Reporting to testributor"
-    # TODO
-  end
+    # TODO: Handle the following case gracefully:
+    # Testributor::Worker#run has already fetched the repo if the commit is not known.
+    # Still, there might be a case that the commit cannot be found even after
+    # pulling the repo. E.g. The history was rewritten somehow (do the commits
+    # get lost then?) or the repo has been reset (deleted and repushed). This is
+    # an edge case but our worker should probably inform katana about this (so
+    # katana can notify the users).
+    def checkout_to_job_commit
+      log "Checking out #{commit_sha}"
+      repo.checkout(commit_sha)
+    end
 
-  def log(message)
-    Testributor.log(message)
+    # Create test database, install needed gems and run any custom build scripts
+    def setup_test_environment
+      log "Setting up environment"
+      Dir.chdir(Testributor::Worker::PROJECT_DIR) do
+        # Inject our gem in the Gemfile
+        # What if there is not Gemfile? (could it be?)
+        `echo 'gem "testributor", group: :test' >> Gemfile`
+
+        # Run custom build commands
+        log `#{build_commands}` if build_commands && build_commands != ''
+      end
+    end
+
+    def report_results
+      log "Reporting to testributor"
+      # TODO
+    end
+
+    def log(message)
+      Testributor.log(message)
+    end
   end
 end
---
+++ lib/testributor/worker.rb
@@ -0,0 +1,55 @@
+module Testributor
+  class Worker
+    # TODO: TBD we might want to sleep longer after many tries with nothing to run
+    # (project developers might be sleeping, no need to keep polling too often)
+    POLLING_TIMEOUT = 3
+    PROJECT_DIR = ENV["HOME"] + '/.testributor'
+
+    attr_reader :api_client, :repo_owner, :repo_name, :github_access_token, :repo
+
+    def initialize(app_id, app_secret)
+      @api_client = Client.new(app_id, app_secret)
+      current_project_response = @api_client.get_current_project
+      # TODO: Consider creating a new model for current_project
+      @repo_owner = current_project_response["repository_owner"]
+      @repo_name = current_project_response["repository_name"]
+      @github_access_token = current_project_response["github_access_token"]
+      @build_commands = current_project_response["build_commands"]
+      create_project_repo
+      @repo = Rugged::Repository.new(PROJECT_DIR)
+    end
+
+    def run
+      while true
+        if (file_response = api_client.fetch_file_to_run)
+          test_job_file = TestJobFile.new(file_response, @repo, @build_commands)
+          fetch_project_repo if !@repo.exists?(test_job_file.commit_sha)
+          Dir.chdir(PROJECT_DIR) do
+            test_job_file.run
+          end
+        else
+          sleep POLLING_TIMEOUT
+        end
+      end
+    end
+
+    private
+
+    def create_project_repo
+      Dir.mkdir(PROJECT_DIR) unless File.exists?(PROJECT_DIR)
+      fetch_project_repo
+    end
+
+    def fetch_project_repo
+      log "Fetching repo"
+      Dir.chdir(PROJECT_DIR) do
+        log `git init`
+        log `git pull https://#{github_access_token}@github.com/#{repo_owner}/#{repo_name}.git`
+      end
+    end
+
+    def log(message)
+      Testributor.log(message)
+    end
+  end
+end
