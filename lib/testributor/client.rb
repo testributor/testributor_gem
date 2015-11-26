17eb1a277b30258c8bda0446441d439d9fb8a09c
Pavlos Kallis <pakallis@gmail.com>
2015-11-26 16:41:54 +0200

    Do not kill gem when server dies

--- lib/testributor/client.rb
+++ lib/testributor/client.rb
@@ -1,14 +1,36 @@
 require 'oauth2'
 require 'securerandom'
+require 'net/http'
 
 module Testributor
   class Client
     attr_reader :token
+    REQUEST_ERROR_TIMEOUT_SECONDS = 10
+    CONNECTION_ERRORS = [Faraday::ConnectionFailed, Net::ReadTimeout]
+
+    # Use this method only when the exception occurs in testributor's side.
+    # In this way, there is no need to restart the gem, each time testributor
+    # server dies. Rescues all exceptions that have to do with katana
+    # communication.
+    def self.ensure_run
+      begin
+        yield
+      rescue *CONNECTION_ERRORS => e
+        # TODO : Send us a notification
+        log "Error occured: #{e.message}"
+        log e.inspect
+        log "Retrying in #{REQUEST_ERROR_TIMEOUT_SECONDS} seconds"
+        sleep(REQUEST_ERROR_TIMEOUT_SECONDS)
+        retry
+      end
+    end
 
     def initialize(app_id, app_secret)
       # Setup connection
       client = OAuth2::Client.new(app_id, app_secret, site: Testributor::API_URL)
-      @token = client.client_credentials.get_token
+      Testributor::Client.ensure_run do
+        @token = client.client_credentials.get_token
+      end
 
       # If no uuid is already set, set it now
       Testributor.uuid ||= SecureRandom.uuid
@@ -30,14 +52,20 @@ module Testributor
 
     private
 
+    def self.log(message)
+      Testributor.log(message)
+    end
+
     # Since we want to add our default header to every request and oauth2
     # does not have a way to add that to token, we wrap the oauth request method
     # to our version that adds the headers
     def request(verb, url, options={})
-      options[:headers] ||= {}
-      options[:headers]["WORKER_UUID"] = Testributor.uuid
+      Testributor::Client.ensure_run do
+        options[:headers] ||= {}
+        options[:headers]["WORKER_UUID"] = Testributor.uuid
 
-      token.request(verb, url, options)
+        token.request(verb, url, options)
+      end
     end
   end
 end
